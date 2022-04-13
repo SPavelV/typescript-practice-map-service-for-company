dcd9cbe93d119f54a3584ce8569e432f4e85bb69
Pavel Solovev <p.solovev@articul.ru>
2022-04-13 08:29:26 +0400

    add points

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,24 +1,41 @@
 import L from 'leaflet';
+import { User } from './User';
+import { Company } from './Company';
 
 export class CustomMap {
   private map: L.Map;
 
   constructor(mapId: string) {
-    this.map = L.map(mapId).setView([51.505, -0.09], 13);
+    this.map = L.map(mapId).setView([0, 0], 2);
 
     L.tileLayer(
       'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGF1bGxlZTIwMjIiLCJhIjoiY2wxdWJuYjk0MDF5ZDNlbzNxZGt1b3hwZCJ9.dUzwlSwfOGP6Eq_mG_ANhg',
       {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox/streets-v11',
         tileSize: 512,
         zoomOffset: -1,
-        accessToken: 'your.mapbox.access.token',
+        accessToken:
+          'pk.eyJ1IjoicGF1bGxlZTIwMjIiLCJhIjoiY2wxdWJuYjk0MDF5ZDNlbzNxZGt1b3hwZCJ9.dUzwlSwfOGP6Eq_mG_ANhg',
       }
     ).addTo(this.map);
   }
-}
 
-// accessToken:  pk.eyJ1IjoicGF1bGxlZTIwMjIiLCJhIjoiY2wxdWJuYjk0MDF5ZDNlbzNxZGt1b3hwZCJ9.dUzwlSwfOGP6Eq_mG_ANhg
+  addUserMarker(user: User): void {
+    L.circle([user.location.lat, user.location.lng], {
+      color: 'red',
+      fillColor: '#ff003',
+      fillOpacity: 0.5,
+      radius: 1000,
+    }).addTo(this.map);
+  }
+
+  addCompanyMarker(company: Company): void {
+    L.circle([company.location.lat, company.location.lng], {
+      color: 'orange',
+      fillColor: '#ffa500',
+      fillOpacity: 0.5,
+      radius: 1000,
+    }).addTo(this.map);
+  }
+}
